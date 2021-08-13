365ed7e3e09895b4147f09bdb554ecf135a9f437
JadenLiuu <r35191007@gmail.com>
2021-08-13 03:46:42 -0700

    featue: subdag

--- dags/dummpy_parallel.py
+++ dags/dummpy_parallel.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.subdag import SubDagOperator
+from subdags.dummy_parallel import dummy_parallel
 
 
 # The args we always defined: start_date, schedule_interval
@@ -11,8 +13,12 @@ default_args = {
 with DAG('dag_id', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
 
     task1 = DummyOperator(task_id='task1')
-    task2 = DummyOperator(task_id='task2')
-    task3 = DummyOperator(task_id='task3')
+    
+    tasks_processing = SubDagOperator(
+        task_id='tasks_processing',
+        dag=dummy_parallel('dag_id', 'tasks_processing', default_args)
+    )
+    
     task4 = DummyOperator(task_id='task4')
 
-    task1 >> [task2, task3] >> task4;
+    task1 >> tasks_processing >> task4;
