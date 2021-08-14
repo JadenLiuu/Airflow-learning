8badcf4c3bbd9610e56a2dd296028f8986d00dcc
JadenLiuuu <r35191007@gmail.com>
2021-08-14 21:47:00 +0800

    feature: taskgroups (#4)

--- dags/dummpy_parallel.py
+++ dags/dummpy_parallel.py
@@ -1,8 +1,7 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.subdag import SubDagOperator
-from subdags.dummy_parallel import dummy_parallel
+from tasks.parallel_tasks import get_group, get_tasks_of_task, get_tasks_in_chain
 
 
 # The args we always defined: start_date, schedule_interval
@@ -13,12 +12,13 @@ default_args = {
 with DAG('dag_id', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
 
     task1 = DummyOperator(task_id='task1')
-    
-    tasks_processing = SubDagOperator(
-        task_id='tasks_processing',
-        dag=dummy_parallel('dag_id', 'tasks_processing', default_args)
-    )
-    
+    tasks_processing = get_group()
     task4 = DummyOperator(task_id='task4')
 
     task1 >> tasks_processing >> task4;
+
+with DAG('task_groups', schedule_interval='@daily', default_args=default_args, catchup=False) as exp:
+    task_groups = get_tasks_of_task()
+
+with DAG('task_groups_chain', schedule_interval='@daily', default_args=default_args, catchup=False) as exp:
+    task_groups_chain = get_tasks_in_chain()
---
+++ dags/tasks/parallel_tasks.py
@@ -0,0 +1,57 @@
+from airflow.utils.task_group import TaskGroup
+from airflow.operators.dummy_operator import DummyOperator
+
+
+def get_group():
+    with TaskGroup('tasks_processing') as tasks_processing:
+        task2 = DummyOperator(task_id='task2')
+        task3 = DummyOperator(task_id='task3')
+
+        return tasks_processing
+
+def get_tasks_of_task():
+    
+    with TaskGroup('tasks_processing') as tasks_of_groups:
+        group = list()
+        with TaskGroup('group1') as group1:
+            task1 = DummyOperator(task_id='task1')
+            task2 = DummyOperator(task_id='task2')
+            task3 = DummyOperator(task_id='task3')
+            task1 >> [task2, task3]
+            group.append(group1)
+
+        with TaskGroup('group2') as group2:
+            task1 = DummyOperator(task_id='task1')
+            task2 = DummyOperator(task_id='task2')
+            task1 >> task2
+            group.append(group2)
+        
+        with TaskGroup('group3') as group3:
+            task1 = DummyOperator(task_id='task1')
+            task2 = DummyOperator(task_id='task2')
+            task1 >> task2
+            group.append(group3)
+
+        [group[0], group[1]] >> group[2]
+        return tasks_of_groups
+
+def get_tasks_in_chain():
+    group = list()
+    for g in range(1,3):
+        with TaskGroup(f'group{g}') as group:
+            ls_subgroup = []
+            for sub_group in range(2):
+                with TaskGroup(f'sub_group{sub_group+1}') as tmp:
+                    task1 = DummyOperator(task_id='task1')
+                    task2 = DummyOperator(task_id='task2')
+                    task1 >> task2
+                    ls_subgroup.append(tmp)
+            task1 = DummyOperator(task_id='task1')
+            task2 = DummyOperator(task_id='task2')
+            task1 >> ls_subgroup >> task2
+            group.append(group)
+
+    for i in range(1, len(group)):
+        group[i-1] >> group[i]
+
+    return group
